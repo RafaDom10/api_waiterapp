4310729665f57bed5dec247bcf6dc7b3836c6675
Rafael Domingues <r-domingues@outlook.com>
2022-11-16 22:48:29 -0300

    added upload images with multer

--- src/app/usecases/categories/listProductsByCategory.ts
+++ src/app/usecases/categories/listProductsByCategory.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express'
 
 import { Category } from '../../models/Category'
 
-export async function listCategories (req: Request, res: Response) {
+export async function listProductsByCategory (req: Request, res: Response) {
   try {
     const categories = await Category.find()
     res.status(200).json(categories)
--- src/router.ts
+++ src/router.ts
@@ -1,17 +1,33 @@
+import path from 'node:path'
 import { Router } from 'express'
+import multer from 'multer'
 
 import { createCategory } from './app/usecases/categories/createCategory'
 import { listCategories } from './app/usecases/categories/listCategories'
+import { listProducts } from './app/usecases/products/listProducts'
+import { createProduct } from './app/usecases/products/createProduct'
+import { listProductsByCategory } from './app/usecases/categories/listProductsByCategory'
 
 export const router = Router()
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination (req, file, callback) {
+      callback(null, path.resolve(__dirname, '..', 'uploads'))
+    },
+    filename (req, file, callback) {
+      callback(null, `${Date.now()}-${file.originalname}`)
+    }
+  })
+})
 
 router.get('/categories', listCategories)
 router.post('/categories', createCategory)
 
-router.get('/products', () => {})
-router.post('/products', () => {})
+router.get('/products', listProducts)
+router.post('/products', upload.single('image'), createProduct)
 
-router.get('/categories/:categoryId/products', () => {})
+router.get('/categories/:categoryId/products', listProductsByCategory)
 
 router.get('/orders', () => {})
 router.post('/orders', () => {})
