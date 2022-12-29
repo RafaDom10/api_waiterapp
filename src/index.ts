455a0bd4d1d5f1fc60d4c45632a86b0635049b0f
Rafael Domingues <r-domingues@outlook.com>
2022-12-29 11:27:48 -0300

    feat: implementing web socket and finished project

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import path from 'node:path'
+import http from 'node:http'
 import express from 'express'
 import mongoose from 'mongoose'
+import { Server } from 'socket.io'
 
 import { router } from './router'
 
+const app = express()
+const server = http.createServer(app)
+export const io = new Server(server)
+
 mongoose.connect('mongodb://localhost:27017')
   .then(() => {
-    const app = express()
-
     app.use((req, res, next) => {
       res.setHeader('Access-Control-Allow-Origin', '*')
       res.setHeader('Access-Control-Allow-Methods', '*')
@@ -18,7 +22,7 @@ mongoose.connect('mongodb://localhost:27017')
     app.use(express.json())
     app.use(router)
 
-    app.listen('3001', () => {
+    server.listen('3001', () => {
       console.log('🔥 Server started at http://localhost:3001')
     })
   })
